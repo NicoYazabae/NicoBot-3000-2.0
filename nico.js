31b42e74a84acfba0502d0bca6508870ecca5543
NicoYazabae <NicoYazabae@gmail.com>
2018-10-16 23:59:22 +0200

    json array test complete

--- nico.js
+++ nico.js
@@ -1,5 +1,6 @@
 const botconfig = require("./botconfig.json");
 const Operators = require("./Operators.json");
+const TestOp = require("./TestOp.json");
 const { Client , Attachment } = require("discord.js");
 const VCID = "474689718425223168"
 let Jimp = require('jimp');
@@ -107,7 +108,41 @@ bot.on("message", async message => {
 
     } }
 
+
+
+    if (cmd === prefix + "OpTest") {
+        let Op = Math.ceil(Math.random()*TestOp.length) - 1;
+
+        let WeaponNum = Math.ceil(Math.random()*TestOp[Op].priweap.length) - 1;
+
+
+        let attachment = new Attachment("./Siege/Operators_icon/" + TestOp[Op].name + ".png" );
+       return message.channel.send("Your Operator is " + TestOp[Op].name + "\nAnd you loadout is: " + (TestOp[Op]).priweap[WeaponNum], attachment);
+
+    }
+
+    if (cmd === prefix + "Op2") {
+        let Num1 = Math.ceil(Math.random()*Operators.length) - 1;
+
+        let Num2 = Math.ceil(Math.random()*Operators.length) - 1;
+
+        if (Num2 === Num1) {
+            let Num2 = Math.ceil(Math.random()*Operators.length) - 1;
+        }
+        else {
+
+
+        let attachment = new Attachment("./Operators/" + Operators[Num1].image + ".png" );
+        let attachment2nd = new Attachment("./Operators/" + Operators[Num2].image + ".png" );
+
+
+
+       return message.channel.send("Your two Operators are: " + Operators[Num1].Name + " and " + Operators[Num2].Name + ".", attachment);
+
+    } }
+
     if (cmd === prefix + "imageTest") {
+
 
         let images = [ './testImage/wallpaper.jpg','./Operators/Ash.png','./Operators/Smoke.png']
         let Jimps = [];
